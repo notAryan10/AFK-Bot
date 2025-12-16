dc2d79236945fb9ffea46cd460f11756820ff94f
Aryan Verma <aryan.verma@adypu.edu.in>
2025-12-16 15:01:10 +0530

    Change port to 5000 and update listen address

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 2323;
+const port = 5000;
 app.get('/', (req, res) => res.send('Afk bot!'));
 
-app.listen(port, () => console.log(`Afk bot is listening to http://localhost:${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Afk bot is listening on port ${port}`));
+
+module.exports = { keep_alive: () => {} };
